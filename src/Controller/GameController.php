ef5d4e7fde09350f4517ba5ba8dc5c89f4714ad8
Mysh3ll <peacefulloijkn@gmail.com>
2019-09-28 20:48:37 +0200

    Display top scores from database

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -2,18 +2,28 @@
 
 namespace App\Controller;
 
+use App\Repository\ScoreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GameController extends AbstractController
 {
     /**
-     * @Route("/", name="game")
+     * @Route("/", name="game_home")
+     * @param ScoreRepository $scoreRepository
+     * @return Response
      */
-    public function index()
+    public function home(ScoreRepository $scoreRepository)
     {
+        // Récupération des 5 meilleurs scores
+        $scores = $scoreRepository->findBy([], ['score' => 'ASC'],5);
+
+        /* Affichage de la page templates/game/index.html.twig
+         * avec les meilleurs scores
+         */
         return $this->render('game/index.html.twig', [
-            'controller_name' => 'GameController',
+            'scores' => $scores,
         ]);
     }
 }
