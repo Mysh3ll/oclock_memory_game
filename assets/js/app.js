a5207c81e7d8856b7c702c85351a3ebcd05aeb4e
Mysh3ll <peacefulloijkn@gmail.com>
2019-09-28 22:52:48 +0200

    Add comments

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,70 +1,80 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you require will output into a single css file (app.css in this case)
+// Importation de la feuille de style scss
 import '../scss/app.scss';
 
-// loads the jquery package from node_modules
+// Chargement du package jquery depuis le répertoir node_modules
 import $ from 'jquery';
 
-// loads the bootstrap package from node_modules
+// Chargement du package bootstrap depuis le répertoire node_modules
 import bootstrap from "bootstrap";
 
 // Exécuter quand le document est ready
 $(document).ready(function () {
 
+    // Notre application avec toutes ses métohdes
     const app = {
         cardValues: [1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1010, 1011, 1012, 1013, 1014, 1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1010, 1011, 1012, 1013, 1014],
         startTime: 0,
         timeout: 0,
 
+        // Initiation de notre application
         init: () => {
+            // On mélange les cartes
             app.shuffleCards();
+            // On lance le jeu
             app.startGame();
         },
         startGame: () => {
             $('.container').show();
-            app.progressBar(600, 600, $('#progress-bar'));
+            // Affichage de la barre de progression
+            app.progressBar(300, 300, $('#progress-bar'));
+            // Gestion des clicks sur les cartes
             app.clickHandlerCard();
+            // Lancement du temps d'origine pour le chronomètre
             app.startTime = new Date();
-            console.log(app.startTime);
         },
         clickHandlerCard: () => {
+            // Gestion du click sur une carte
             $('.card').on('click', function () {
+                // On fait tourner la carte sélectionnée
                 app.flipCard($(this));
             });
         },
         shuffleCards: () => {
-            // mélange le tableau
+            // Mélange le tableau "cardValues"
             app.cardValues.sort(() => Math.random() - 0.5);
-            // ajoute les div de cartes
+            // Ajoute les div de cartes dans la div container
             app.cardValues.forEach(() => $('.container').append('<div class="card unselected"></div>'));
-            // ajoute l'attribut data-card
+            // Ajoute l'attribut data-card à chaque carte
             $('.card').each(function (index) {
                 $(this).attr('data-card', app.cardValues[index]);
             });
         },
         flipCard: (card) => {
+            // Ajoute la classe 'selected' à la carte sélectionnée
             card.addClass('selected');
+            // Affiche une image quand la carte est retournée
             card.css({'background-image': 'url("https://picsum.photos/id/' + card.data('card') + '/113/150")'});
+            // Lorsque deux cartes sont sélectionnées
             if ($('.selected').length === 2) {
+                // Vérification de la correspondance entre les deux cartes
                 app.checkMatchCards();
             }
         },
         checkMatchCards: () => {
+            // Création d'une constante pour éviter la répétition dans le code
             const selected = $('.selected');
+            // Si les deux cartes sont identiques on les efface
             if (selected.first().data('card') === selected.last().data('card')) {
+                // On lance l'animation d'effacement
                 setTimeout(() => {
                     $('.selected').each(function () {
                         $(this).removeClass('unselected selected').css({'background': ''}).animate({opacity: 0}, {duration: 1000});
+                        // On contrôle si on a fini le jeu
                         app.checkWinGame();
                     });
                 }, 1000);
             } else {
+                // Si les deux cartes sont différentes on les retourne
                 setTimeout(() => {
                     $('.selected').each(function () {
                         $(this).removeClass('selected').css({'background': ''});
@@ -73,14 +83,18 @@ $(document).ready(function () {
             }
         },
         checkWinGame: () => {
+            // On verifie s'il ne reste plus de carte dans le jeu
             if ($('.unselected').length === 0) {
+                // Création d'une constante pour le chronomètre de fin de jeu
                 const endTime = new Date();
+                // Calcul entre le début et la fin en secondes
                 const time = Math.round((endTime - app.startTime) / 1000);
+                // Affichage de la fenêtre de fin de jeu
                 app.endGame('Hey Good Job ! Tu as gagné en ' + time + ' secondes', true, time);
-                // console.log('Tu as gagné en ' + time + ' seconds');
             }
         },
         progressBar: (timeleft, timetotal, $element) => {
+            // Gestion de l'affichage de la barre de progression
             const progressBarWidth = timeleft * $element.width() / timetotal;
             $element.find('div').animate({width: progressBarWidth}, 500).html(Math.floor(timeleft / 60) + ":" + timeleft % 60);
             if (timeleft >= 0) {
@@ -88,27 +102,33 @@ $(document).ready(function () {
                     if (timeleft !== 0) {
                         app.progressBar(timeleft - 1, timetotal, $element);
                     } else {
+                        // Le temps est écoulé, retour au début du jeu
                         app.endGame('Allez retente ta chance !!!', false);
                     }
                 }, 1000);
             }
         },
         endGame: (text, win, time = 0) => {
+            // Blocage de la barre de progression
             clearTimeout(app.timeout);
+            // Affichage de la fenêtre de fin de jeu
             $('.end-game').css('display', 'flex');
             $('.end-game p').text(text);
-            // on relance la partie
+            // On relance la partie
             $('#end-game').on('click', () => {
+                // Si on a fini le jeu on sauvegarde le temps en base de données via une requête ajax
                 if (win) {
                     app.saveScoreToDatabase(time);
                 } else {
+                    // Ou sinon on recharge la page
                     location.reload();
                 }
             });
         },
         saveScoreToDatabase: (time) => {
+            // Récupération du path afin d'effectuer la requête ajax
             const path = $('#save-score').data('path').replace(/.$/, time);
-
+            // Exécution de la requête ajax
             $.ajax({
                 url: path,
                 type: 'post',
@@ -126,7 +146,9 @@ $(document).ready(function () {
 
     // Start Game
     $('#start-game').on('click', () => {
+        // On cache l'affichage des scores
         $('.top-scores').hide();
+        // On lance l'application
         app.init();
     });
 });
--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -17,6 +17,8 @@ use Symfony\Component\Routing\Annotation\Route;
 class GameController extends AbstractController
 {
     /**
+     * Controller pour la page d'accueil du jeu
+     *
      * @Route("", name="home")
      * @param ScoreRepository $scoreRepository
      * @return Response
@@ -26,16 +28,17 @@ class GameController extends AbstractController
         // Récupération des 5 meilleurs scores
         $scores = $scoreRepository->findBy([], ['score' => 'ASC'], 5);
 
-        /* Affichage de la page templates/game/index.html.twig
-         * avec les meilleurs scores
-         */
+        // Affichage de la page templates/game/index.html.twig avec les meilleurs scores
         return $this->render('game/index.html.twig', [
             'scores' => $scores,
         ]);
     }
 
     /**
-     * @Route("save-score/{timeInSeconds}", name="save_score", requirements={"timeInSeconds"="\d+"})
+     * Controller qui permet de gérer la requête ajax depuis le javascript
+     * pour sauvegarder le score en base de données
+     *
+     * @Route("save-score/{timeInSeconds}", name="save_score", methods={"POST"}, requirements={"timeInSeconds"="\d+"})
      * @param Request $request
      * @param EntityManagerInterface $em
      * @param int $timeInSeconds
@@ -43,18 +46,26 @@ class GameController extends AbstractController
      */
     public function saveScore(Request $request, EntityManagerInterface $em, int $timeInSeconds)
     {
+        // On vérifie si la requête correspond à de l'ajax
         if ($request->isXmlHttpRequest()) {
 
+            // Création de l'obejt Score
             $score = new Score();
+            // Attribution du temps
             $score->setScore($timeInSeconds);
 
+            // On persist l'objet
             $em->persist($score);
+            // On sauve en base de données
             $em->flush();
 
+            // On renvoie une réponse en json pour la requête ajax
             $result['message'] = 'Score sauvegardé';
             return $this->json($result);
 
         }
+
+        // On renvoie une réponse en json pour la requête ajax
         $result['message'] = 'Une erreur s\'est produite';
         return $this->json($result);
     }
